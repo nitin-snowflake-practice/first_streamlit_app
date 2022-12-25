0e8a4bd9374581280988ecb5360a369f3bfa1f6d
nitin-snowflake-practice <121391316+nitin-snowflake-practice@users.noreply.github.com>
2022-12-25 20:29:49 +0530

    Update streamlit_app.py
    added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title('My parents New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.test('Omega 3 & Blueberry Oatmeal')
+streamlit.test('Kale, Spinach & Rocket Smoothie')
+streamlit.test('Hard-Boiled Free-Range Egg')
