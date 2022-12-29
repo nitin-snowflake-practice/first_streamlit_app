12e3bf134718df654294d051a0d42550835485ba
nitin-snowflake-practice <121391316+nitin-snowflake-practice@users.noreply.github.com>
2022-12-30 00:10:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,8 +45,6 @@ if streamlit.button('Get fruit load list'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
-
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list values ('from streamlit')")
@@ -57,4 +55,6 @@ if streamlit.button('Add a fruit to the list'):
   back_from_function = insert_row_snowflake(add_my_fruit) 
   streamlit.text(back_from_function)
 streamlit.write('The user entered ', add_my_fruit)
+
+streamlit.stop()
 
