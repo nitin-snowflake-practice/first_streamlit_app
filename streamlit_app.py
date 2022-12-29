1d0e316382516d8dc6ccf06e9d83773c72691950
nitin-snowflake-practice <121391316+nitin-snowflake-practice@users.noreply.github.com>
2022-12-30 00:01:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,8 @@ def get_fruit_load_list():
 if streamlit.button('Get fruit load list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
-streamlit.dataframe(my_data_rows)
+  streamlit.dataframe(my_data_rows)
+
 streamlit.stop()
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit') 
